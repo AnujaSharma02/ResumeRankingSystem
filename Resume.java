eaf246a9d3f4f27add68ea4bf2be995befceae36
Anuja Sharma <120904105+AnujaSharma02@users.noreply.github.com>
2025-04-30 19:20:33 +0530

    Create Resume.java

---
+++ Resume.java
@@ -0,0 +1,9 @@
+public class Resume {
+    String filename;
+    int score;
+
+    public Resume(String filename, int score) {
+        this.filename = filename;
+        this.score = score;
+    }
+}
